6b8aa89545f7320705a1a1ac0dc5ef7ed65a8930
wmather123 <22200231@bucks.ac.uk>
2022-10-20 22:53:23 +0100

    Week 1 and 2

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -14,10 +14,37 @@ public class Week1
         System.out.println();
         System.out.println("  Week 1 Java Basic Input/Output ");
         System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println(" by William Mather");
         System.out.println();
 
         String name = InputReader.getString("Enter your name > ");
         System.out.println("Hello " + name);
+
+
+        System.out.println("insert the Length and Height of your shape!");
+        int length = InputReader.getInt("Length:");
+        int height = InputReader.getInt("height:");
+        int perimeter = (2 * height) + (2 * length);
+        int area = height + length;
+        System.out.println("the perimeter is: " + perimeter + " The Area is: " + area);
+
+        System.out.println("Enter two values and then we will find the average!");
+        double numberOne = InputReader.getDouble("Enter the first number: ");
+        double numberTwo = InputReader.getDouble("Enter the second Number: ");
+        double average = (numberOne + numberTwo) / 2;
+        System.out.println("The average is " + average);
+
+
+        int orderedNumberOne = InputReader.getInt("Enter the first Value: ");
+        int orderedNumberTwo = InputReader.getInt("Enter the second value: ");
+        System.out.println("Your numbers were: " + orderedNumberOne + " " + orderedNumberTwo);
+        System.out.println("Your numbers were also: " + orderedNumberTwo + " " + orderedNumberOne);
+
+        System.out.println("Enter your age!");
+        int age = InputReader.getInt("Age: ");
+        System.out.println("You were born in the year: " + (2022 - age));
+
+
+
     } 
 }
--- src/Week2/Week02.java
+++ src/Week2/Week02.java
@@ -1,6 +1,8 @@
 package Week2;
 
-public class Week02 
+import helpers.InputReader;
+
+public class Week02
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
     public static void main(String[] args)
@@ -13,7 +15,31 @@ public class Week02
         System.out.println();
         System.out.println("  Week 2 Java Selection & Iteration ");
         System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println(" by William Mather");
         System.out.println();
-    }     
+
+        String grade = InputReader.getString("Enter your grade: ");
+
+        switch (grade) {
+        case "A": System.out.println("This is a 1st"); break;
+        case "B": System.out.println("This is a 2:1"); break;
+        case "C": System.out.println("This is a 2:2"); break;
+        case "D": System.out.println("This is a 3rd"); break;
+        case "E": System.out.println("This is an ordinary"); break;
+        case "F": System.out.println("This is a fail"); }
+
+        for (int digits = 0; digits <= 9; digits++) {
+            System.out.println(digits);
+        }
+
+        int multiplicand;
+        int multiplier = InputReader.getInt("Enter the number you would like to see the times table of: ");
+        for (multiplicand = 1; multiplicand <= 12; multiplicand++) {
+            System.out.print(multiplicand + " x " + multiplier + " = ");
+            System.out.println(multiplicand * multiplier);
+
+
+        }
+
+    }
 }
