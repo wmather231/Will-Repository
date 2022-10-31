92ebe5e145ef87a97c9eaca4df68807628bb02e0
wmather123 <22200231@bucks.ac.uk>
2022-10-31 17:54:01 +0000

    Add song finished

--- src/Assesment/Assessment.java
+++ src/Assesment/Assessment.java
@@ -2,34 +2,56 @@ package Assesment;
 
 import helpers.InputReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
 
 public class Assessment {
-
+    //Creating the Array List and the Song class
+    private static final ArrayList<Song> songs = new ArrayList<Song>();
     public static void main(String[] args)
     {
 
-
-        HashMap<String, Integer> songs = new HashMap<String, Integer>();
-        songs.put("The pretender, foo fighters", 561068936);
-        songs.put("Me and Michael, MGMT", 49622743);
-        songs.put("Chop Suey!, System Of A Down", 893895854);
-        songs.put("Simple Song, The Shins", 106788827);
-        songs.put("Fugazi, Marillion", 206540);
+        boolean runProgramme = true;
 
 
+        while (runProgramme == true) {
+        //adding the initial songs to the programme
+        Song song = new Song("The pretender", "Foo Fighters", 561068936);
+        song = new Song("Me and Michael", "MGMT", 49622743);
+        song = new Song("Chop Suey!", "System Of A Down", 893895854);
+        song = new Song("Simple Song", "The Shins", 106788827);
+        song = new Song("Fugazi", "Marillion", 206540);
 
+
+        //Presenting the user with the options of the programme
+        String newline = System.lineSeparator();
+        System.out.println("1. Add a song" + newline + "2. remove a song" + newline + "3. List the songs" + newline + "4. list the songs in the order of plays" + newline + "5. End the programme");
         String options = InputReader.getString("Select the action you would like to do:");
         switch (options) {
-            case "1": addSong(); break;
-            case "2": removeSong(); break;
-            case "3": listSongs(); break;
-            case "4": listSpecificSongs();
-        } //end of switch
+            case "1":
+                addSong();
+                break;
+            case "2":
+                removeSong();
+                break;
+            case "3":
+                listSongs();
+                break;
+            case "4":
+                listSpecificSongs();
+                break;
+            case "5":
+                runProgramme = false;
 
+        } //end of switch
+    }//end of while loop
     }
-    public static void addSong() {
+    public static String addSong() { //the function for adding songs
+        String title = InputReader.getString("Enter the song name: ");
+        String artist = InputReader.getString("Enter the artist of the song: ");
+        int playCount = InputReader.getInt("Enter the playCount of the song: ");
+
+        Song song = new Song(title, artist ,playCount);
+        return title + " " + artist + " " + playCount;
+
     }
     public static void removeSong() {
     }
--- src/Assesment/Song.java
+++ src/Assesment/Song.java
@@ -1,5 +1,17 @@
 package Assesment;
 
 public class Song {
+    //creating the different variables for the array list
+    private String title;
+    private String artist;
+    private int playCount;
+
+    public Song(String title, String artist, int playCount) { //Allowing the array list to add the title, artist, and play count to the list
+        this.title = title;
+        this.artist = artist;
+        this.playCount = playCount;
+//Then printing the current list
+        System.out.println(title + " " + artist + " " + playCount);
+    }
 
 }
